4fff4f633cfb4855f02f1f503d3490392468dacf
student <student@C246U53>
2020-09-03 15:18:52 +0300

    111

---
+++ Praktika1_Zadanie1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Praktika1_Zadanie1
+{
+  class Program
+  {
+    
+    struct Person
+    {
+      public string firstName;
+      public string lastName;
+      public int age;
+      public enum Genders : int { Male, Female };
+      public Genders gender;
+      public Person(string _firstName, string _lastName, int _age, Genders _gender)
+      {
+        firstName = _firstName;
+        lastName = _lastName; 
+        age = _age;
+        gender = _gender;
+      }
+      public override string ToString()
+      {
+        return firstName + " " + lastName + " (" + gender + "), age " + age;
+      }
+    }
+    static void Main(string[] args)
+    {
+      string sk = "Microsoft .NET Framework 2 Application Development Foundation";
+      string[] sa = sk.Split('.');
+      foreach 
+      Console.WriteLine(sa);
+      SByte a = 0;
+      Byte b = 0;
+      Int16 c = 0;
+      Int32 d = 0;
+      Int64 e = 0;
+      string s = "";
+      Exception ex = new Exception();
+      object[] types = { a, b, c, d, e, s, ex };
+      Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+      Console.WriteLine(p);
+      foreach (object o in types)
+      {
+        string type;
+        if (o.GetType().IsValueType) type = "Value type";
+        else
+          type = "Reference Type";
+        Console.WriteLine("{0}: {1}", o.GetType(), type);
+      }
+      //Console.WriteLine("Hello World!");
+    }
+  }
+}
